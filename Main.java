f4c30804833c6a744ed87295849a99e3b384a022
KevinDom0530 <137342710+KevinDom0530@users.noreply.github.com>
2023-06-21 23:01:19 +0000

    Accept the assignment

--- Main.java
+++ Main.java
@@ -1,11 +1,7 @@
-import java.util.Scanner;
-
 class Main {
   public static void main(String[] args) {
-    System.out.println("Enter a word:");
-    Scanner scanner = new Scanner(System.in);
-    String input = scanner.nextLine();
-    System.out.println(input);
+    System.out.println("Hello world!");
+    
     //Your code will return the length of the entered String
   }
 }
