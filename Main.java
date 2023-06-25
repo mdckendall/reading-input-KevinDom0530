6dd26e775564498f55912a9bb9b440fcb6e54a41
KevinDom0530 <137342710+KevinDom0530@users.noreply.github.com>
2023-06-25 19:08:47 +0000

    Assignment submt, please accept

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 class Main{
   public static void main(String[] args){
- System.out.println("Hello, world!");   
+ Scanner input = new Scanner();
+    System.out.println("Enter a word: ");
+    String input = input.nextString();
+    System.out.println(nextLine());
+    System.out.println(input + input.length());   
   }
 }
